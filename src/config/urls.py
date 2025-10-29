67976d4a71be4cd786503c7c35faace54aead21b
Luis Lessa <prof.lpjunior@gmail.com>
2025-10-29 19:00:36 -0300

    initialize Django project structure with basic settings and app configurations

---
+++ src/config/urls.py
@@ -0,0 +1,13 @@
+"""Senacmon URL configuration."""
+
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('accounts/', include('accounts.urls')),
+    path('game/', include('game.urls')),
+    path('wallet/', include('wallet.urls')),
+]
