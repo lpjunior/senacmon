4e61aa670d53b02304dee09562c73f5316609630
Luis Lessa <prof.lpjunior@gmail.com>
2025-11-12 22:12:05 -0300

    carrega .env apenas em ambiente de desenvolvimento

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,10 +1,17 @@
 import os
 from pathlib import Path
-from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR.parent / '.env')  # ../.env
+
+# Carrega .env APENAS no DEV
+if os.getenv("DJANGO_ENV") != "production":
+    try:
+        from dotenv import load_dotenv
+        load_dotenv(BASE_DIR.parent / ".env")
+    except ImportError:
+        pass
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
