bb683bcf6b19c0a65235a3c9eee89ba0b25ca920
Luis Lessa <prof.lpjunior@gmail.com>
2025-11-17 10:39:13 -0300

    atualiza URLs de redirecionamento de login e logout para o namespace 'common'

--- src/config/settings.py
+++ src/config/settings.py
@@ -120,7 +120,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'common:home'
+LOGOUT_REDIRECT_URL = 'common:home'
 
 AUTH_USER_MODEL = "auth.user"
